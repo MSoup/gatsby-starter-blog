1bb0e3bc5807dfdb941a584e0959012ffc45f0cf
Daven L <davenlu1989@gmail.com>
2021-10-12 12:19:52 +0900

    removed hero text, embedded into index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,24 @@ import Seo from "../components/seo"
 import { StaticImage } from "gatsby-plugin-image"
 
 
+const Excerpt = () => {
+  return (
+    <div>
+      <p>A mere two keys.</p>
+      <p>The holy grail of shortcuts.</p>
+      <p>I chose the name <strong>ALT F4</strong> for my blog as a way to relay a witty message to thee: <strong>I switched my career from being an English Teacher (read: Assistant Language Teacher, or...ALT) to becoming a software developer. </strong> Hence, ALT. Hence, you know, "quitting" being an ALT? Ho ho ho, and now that I have your attention, I'll have you know that I spent several months brooding over a name for my blog. Welcome. </p>
+    </div>
+  )
+}
+
 const LandingPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
 
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="Home" />
-      <p>This site is still in progress. If you're reading this, congrats. You're within the first 10 visitors.</p>
+      <Excerpt />
+      <p>If you've read this far, congrats. You're within the first 10 visitors.</p>
       <h3>Let me reward you with a cat.</h3>
       <StaticImage 
         alt="I'm a cat"
